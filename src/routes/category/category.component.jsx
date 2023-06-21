1b70aab3e0cd164910e7111871f0d3db437b51bc
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-21 04:26:44 -0400

    Updating components for graph ql

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
+import { gql, useQuery } from "@apollo/client";
 
 import ProductCard from '../../components/product-card/product-card.component';
 import Spinner from '../../components/spinner/spinner.component';
@@ -8,9 +9,39 @@ import { CategoriesContext } from '../../contexts/categories.context';
 
 import { CategoryContainer, Title } from './category.styles';
 
+const GET_CATEGORY = gql`
+  getCollectionsByTitle(title: $title) {
+    id
+    title
+    items {
+      id
+      name
+      price
+      imageUrl
+    }
+  }
+`
+
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
+  // const { categoriesMap, loading } = useContext(CategoriesContext);
+
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category
+    }
+  });
+
+  useEffect(() => {
+    if (data) {
+      const {
+        getCollectionsByTitle: { items }
+      } = data;
+
+      setProducts(items);
+    }
+  }, [category, data]);
+
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
