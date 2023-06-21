7c03745161fd0b5f68cd20f912cf65779a5dfdd2
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-21 04:06:14 -0400

    Initializing graph ql

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
@@ -6,17 +7,51 @@ export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    } 
+  }
+`
+
 export const CategoriesProvider = ({ children }) => {
+  const {loading, error, data} = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+    if (data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
 
-    getCategoriesMap();
-  }, []);
+        return acc;
+      }, {});
+
+      setCategoriesMap(collectionsMap);
+    }
+
+  }, [data]);
+
+  console.log("loading: ", loading);
+  console.log("data: ", data);
+
+  // useEffect(() => {
+  //   const getCategoriesMap = async () => {
+  //     const categoryMap = await getCategoriesAndDocuments();
+  //     setCategoriesMap(categoryMap);
+  //   };
+
+  //   getCategoriesMap();
+  // }, []);
 
   const value = { categoriesMap };
   return (
